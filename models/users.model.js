8bfc04c7191b84f55a8ad61e9f59d877c1b59200
Lundberg <kevin.lundberg@sweco.se>
2022-08-25 22:47:49 +0200

    updated usersmodel

--- models/users.model.js
+++ models/users.model.js
@@ -14,7 +14,7 @@ async function addUser(id, username) {
     return false;
   }
 
-  const sql = "INSERT INTO users (id, username) VALUES (?, ?)";
+  const sql = "INSERT INTO wd22jwt5qwm6b4d3.users (id, username) VALUES (?, ?)";
   return new Promise((resolve, reject) => {
     db.query(sql, [id, username], (error) => {
       if (error) {
@@ -28,7 +28,7 @@ async function addUser(id, username) {
 
 // GET ALL USERS
 function getUsers() {
-  const sql = "SELECT * FROM users";
+  const sql = "SELECT * FROM wd22jwt5qwm6b4d3.users";
   return new Promise((resolve, reject) => {
     db.query(sql, (error, rows) => {
       if (error) {
@@ -44,7 +44,7 @@ function getUsers() {
 function getOneUser(id, username) {
   console.log(id);
   console.log(username);
-  const sql = "SELECT * FROM users WHERE id = ? AND name = ?";
+  const sql = "SELECT * FROM wd22jwt5qwm6b4d3.users WHERE id = ? AND name = ?";
   return new Promise((resolve, reject) => {
     db.get(sql, [id, username], (error, rows) => {
       if (error) {
@@ -58,7 +58,8 @@ function getOneUser(id, username) {
 
 // UPDATE USERS ACTIVE ROOM / BETTER SOLUTION?
 function updateActiveRoom(roomName, username) {
-  const sql = "UPDATE users SET active_room = ? WHERE username = ?";
+  const sql =
+    "UPDATE wd22jwt5qwm6b4d3.users SET active_room = ? WHERE username = ?";
   return new Promise((resolve, reject) => {
     db.query(sql, [roomName, username], (error) => {
       if (error) {
@@ -71,7 +72,8 @@ function updateActiveRoom(roomName, username) {
 }
 
 function removeActiveRoom(roomName) {
-  const sql = "UPDATE users SET active_room = null WHERE active_room = ?";
+  const sql =
+    "UPDATE wd22jwt5qwm6b4d3.users SET active_room = null WHERE active_room = ?";
   return new Promise((resolve, reject) => {
     db.query(sql, roomName, (error) => {
       if (error) {
@@ -86,7 +88,8 @@ function removeActiveRoom(roomName) {
 // GET ONE USER
 function getUsersInRoom(roomName) {
   console.log(roomName);
-  const sql = "SELECT username FROM users WHERE active_room = ?";
+  const sql =
+    "SELECT username FROM wd22jwt5qwm6b4d3.users WHERE active_room = ?";
   return new Promise((resolve, reject) => {
     db.query(sql, [roomName], (error, rows) => {
       if (error) {
@@ -100,7 +103,7 @@ function getUsersInRoom(roomName) {
 
 // DELETE ALL CLIENT CONNECTED USERS
 function deleteUsers(clientID) {
-  const sql = "DELETE from users where id = ?";
+  const sql = "DELETE from wd22jwt5qwm6b4d3.users where id = ?";
   return new Promise((resolve, reject) => {
     db.query(sql, [clientID], (error) => {
       if (error) {
@@ -114,7 +117,7 @@ function deleteUsers(clientID) {
 
 // DELETE ALL USERS
 function deleteAllUsers() {
-  const sql = "DELETE from users";
+  const sql = "DELETE from wd22jwt5qwm6b4d3.users";
   return new Promise((resolve, reject) => {
     db.query(sql, (error) => {
       if (error) {
